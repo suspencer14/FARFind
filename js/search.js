291dc377350681f9b254cfdf05feeca6e6eaf307
Spencer DeLange <spencer.delange@gmail.com>
2021-12-16 15:49:00 -0700

    initial commit

---
+++ js/search.js
@@ -0,0 +1,8 @@
+fetch('./search.html')
+    .then(res => res.text())
+    .then(text => {
+        let oldelem = document.querySelector("script#replace_with_search");
+        let newelem = document.createElement("div");
+        newelem.innerHTML = text;
+        oldelem.parentNode.replaceChild(newelem,oldelem);
+    })
